4be766edde98a6bf7459bdeb25e41e95b94046d5
Khong Ming Liang <khongmingliang@icloud.com>
2021-03-03 21:14:01 +0800

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Ming Liang Khong on 3/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
